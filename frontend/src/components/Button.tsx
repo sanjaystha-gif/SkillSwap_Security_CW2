52c375d5b15422b88151f1143f5d88dd426ba9bb
sanjaystha-gif <bahasanjay27@gmail.com>
2026-08-02 13:45:25 +0545

    Default button type to button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -6,6 +6,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function Button({
+  type = 'button',
   variant = 'primary',
   loading = false,
   className = '',
